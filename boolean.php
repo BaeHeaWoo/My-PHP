9050c38f8a098fd44f11b26ce9dcda0201783ea2
BaeHeaWoo <30643699+BaeHeaWoo@users.noreply.github.com>
2021-06-10 01:24:34 +0900

    upload
    if, repeat, test, boolean

---
+++ boolean.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<h1>Boolean&Comparison</h1>
+	<h3>혼자보다 비교 연산자와의 시너지가 좋다</h3>
+	<?php var_dump(1); echo"<br>"; ?>  <!-- var_dump()괄호안에 타입을 알려준다 -->
+	<?php var_dump('11'); echo "<br>";?> <!-- string타입 이고 문자열 2개로 구성되어 있다. -->
+	<?php var_dump(1==1); echo "1==1 <br>" ?> <!-- 좌항과 우항이 같으면 true 다르면 false를 반환한다. 비교 연산자(==)를 통해 값 인 true false (boolean) -->
+	<?php var_dump(1>1); echo "1>1 그외 >, >=, <= 등이 있다<br>" ?>   <!--부등호 연산도 가능하다 결과는 boolean타입이 반환된다-->
+	
+</body>
+</body>
+</html>
